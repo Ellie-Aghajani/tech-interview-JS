e26de39ebbec8aa8599ab4815c940862646131cc
Ellie-Aghajani <z.aqajani@gmail.com>
2024-08-09 16:52:57 -0700

    logic to solve the challenge

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+//swap variables
+//1: declare another variable and use it as a backup
+//2: put whatever we 're storing in 'a' in the backup variable
+//3: copy 'b' to 'a'
+//4: copy backup to 'b'
+
 let a = 'red';
 let b = 'blue';
 
